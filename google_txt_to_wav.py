525abf8972a9de00b813e2eb1dd77454907ed4d9
mxav1111 <mxav1111@gmail.com>
2024-02-28 16:29:45 -0800

    Update google_txt_to_wav.py
     Updated line 67 with change in variable name -->   file_in_buffer = f.read()
    
    Added four lines (73 to 76)
    file_in_buffer=re.sub(r'(?:=!pgB!=.*=!Epg!=\n)+',r'',file_in_buffer)
    file_in_buffer=re.sub(r'\([^ ]\)-(\n)+',r'\1',file_in_buffer) ## word that splits and spans line using hypen is merged
    file_in_buffer=re.sub(r'([^ ])-\n',r'\1',file_in_buffer) ## remove new lines and convert multi spaces to single.
    file_in_buffer=re.sub(r'(\n)+',r' ',file_in_buffer); file_in_buffer=re.sub(r'\n',r' ',file_in_buffer); file_in_buffer=re.sub(r'[ ][ ]*',r' ',file_in_buffer)  ## remove new lines and convert multi spaces to single.

--- google_txt_to_wav.py
+++ google_txt_to_wav.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 from dotenv import load_dotenv
 from google.cloud import storage, texttospeech
 
@@ -66,9 +66,15 @@ print(f"processing {text_file_to_read} --> {audio_file_to_write}")
 print(f"Using: {langcode} : {voicename} : {speakingrate}")
 
 with open(text_file_to_read, "r", encoding="utf-8") as f:
-    fc = f.read()
+    file_in_buffer = f.read()
 
 f = open(audio_file_to_write, "w", encoding="utf-8")
+
+file_in_buffer=re.sub(r'(?:=!pgB!=.*=!Epg!=\n)+',r'',file_in_buffer)
+file_in_buffer=re.sub(r'\([^ ]\)-(\n)+',r'\1',file_in_buffer) ## word that splits and spans line using hypen is merged
+file_in_buffer=re.sub(r'([^ ])-\n',r'\1',file_in_buffer) ## remove new lines and convert multi spaces to single.
+file_in_buffer=re.sub(r'(\n)+',r' ',file_in_buffer); file_in_buffer=re.sub(r'\n',r' ',file_in_buffer); file_in_buffer=re.sub(r'[ ][ ]*',r' ',file_in_buffer)  ## remove new lines and convert multi spaces to single.
+
 
 if blob_exists(bktnm, audio_file_to_write):
     print(f"{audio_file_to_write} exists in {bktnm}, removing first..")
